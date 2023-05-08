6244b68cb7018a45430a2ca8db442a84418633c6
Kohki-Takatama <koh.takatama@gmail.com>
2023-05-08 21:26:11 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UseState</h1>
+      {/* データの状態が変わったときにページ（の差分）をレンダリングする（レンダリングしないと内部的な更新に留まってしまう） */}
+      <button onClick={handleClick}>+</button>
+      <p>{count}</p>
     </div>
   );
 }
